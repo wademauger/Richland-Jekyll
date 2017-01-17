970a0a93153bdb100c331986641d6588d6c6aea3
Wade Mauger <wadeanthony0100@gmail.com>
2017-01-17 09:10:47 -0500

    Update script for CI

--- script/postdeploy.sh
+++ script/postdeploy.sh
@@ -6,16 +6,21 @@ curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-stand
 
 if [[ $string == *"darwin"* ]]; then
   # download and unpack the OSX version
-  echo "OSX detected, downloading chrome driver for Mac OS..."
+  echo "OSX detected, downloading chromedriver for Mac OS..."
   curl -L https://chromedriver.storage.googleapis.com/2.27/chromedriver_mac64.zip | funzip  > chromedriver
+  chmod +x chromedriver
+  ./chromedriver &
+elif [[ $CI ]]; then
+  # CircleCI ships with chromedriver v27
+  echo "CI runtime detected, skipping chromedriver download..."
+  chromedriver
 else
   # download and unpack the Linux version
-  echo "Linux detected, downloading chrome driver for 64 Bit Linux OS's..."
+  echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
   curl -L https://chromedriver.storage.googleapis.com/2.27/chromedriver_linux64.zip | gzip -dc > chromedriver
+  chmod +x chromedriver
+  ./chromedriver &
 fi
-
-chmod +x chromedriver
-./chromedriver &
 
 echo "Starting selenium server..."
 java -jar -Dwebdriver.gecko.driver=./geckodriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
