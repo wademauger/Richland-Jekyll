afc058ec39e6c81c912910b3f5a56de6e54fc305
Wade Mauger <wadeanthony0100@gmail.com>
2017-01-30 11:17:43 -0500

    update deploy

--- script/postdeploy.sh
+++ script/postdeploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -x
+#!/bin/bash +x
 
 chromeDriverVersion=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 cd webdriverio-test
@@ -10,16 +10,16 @@ if [ "$(uname)" == "Darwin" ]; then
   curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_mac64.zip | funzip  > chromedriver
   chmod +x chromedriver
   ./chromedriver &
+elif [[ $CI ]]; then
+  # CircleCI ships with chromedriver v27
+  echo "CI runtime detected, skipping chromedriver download..."
+  chromedriver &
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # download and unpack the Linux version
   echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
   curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_linux64.zip | gzip -dc > chromedriver
   chmod +x chromedriver
   ./chromedriver &
-elif [[ $CI ]]; then
-  # CircleCI ships with chromedriver v27
-  echo "CI runtime detected, skipping chromedriver download..."
-  chromedriver &
 fi
 
 echo "Starting selenium server..."
