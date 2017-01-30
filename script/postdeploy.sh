4af40b3e7283ba83211392479f3a94cdc18a34b3
Wade Mauger <wadeanthony0100@gmail.com>
2017-01-30 11:47:21 -0500

    Update deploy logic

--- script/postdeploy.sh
+++ script/postdeploy.sh
@@ -10,22 +10,25 @@ if [ "$(uname)" == "Darwin" ]; then
   curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_mac64.zip | funzip  > chromedriver
   chmod +x chromedriver
   ./chromedriver &
+  echo "Starting selenium server..."
+  java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
 elif [[ $CI ]]; then
   # CircleCI ships with chromedriver v27
   echo "CI runtime detected, skipping chromedriver download..."
   chromedriver &
+  echo "Starting selenium server..."
+  java -jar -Dwebdriver.chrome.driver=chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
 fi
-  #uncomment to use on linux
+#uncomment to use on linux
 #elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 #  # download and unpack the Linux version
 #  echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
 #  curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_linux64.zip | gzip -dc > chromedriver
 #  chmod +x chromedriver
 #  ./chromedriver &
+#  echo "Starting selenium server..."
+#  java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
 #fi
-
-echo "Starting selenium server..."
-java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
 
 echo "installing node dependancies..."
 npm i
