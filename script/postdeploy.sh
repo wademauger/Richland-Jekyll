431ae6b6007b98a45ac7e0d6fe4c0d3d4f2a34cb
Wade Mauger <wadeanthony0100@gmail.com>
2017-01-30 11:28:09 -0500

    remove linux from deployment for now

--- script/postdeploy.sh
+++ script/postdeploy.sh
@@ -14,13 +14,14 @@ elif [[ $CI ]]; then
   # CircleCI ships with chromedriver v27
   echo "CI runtime detected, skipping chromedriver download..."
   chromedriver &
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  # download and unpack the Linux version
-  echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
-  curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_linux64.zip | gzip -dc > chromedriver
-  chmod +x chromedriver
-  ./chromedriver &
-fi
+  #uncomment to use on linux
+#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+#  # download and unpack the Linux version
+#  echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
+#  curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_linux64.zip | gzip -dc > chromedriver
+#  chmod +x chromedriver
+#  ./chromedriver &
+#fi
 
 echo "Starting selenium server..."
 java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
