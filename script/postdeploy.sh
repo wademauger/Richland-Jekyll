ae7348a9fe1d97db8d73d58ae0a7bb528f73a983
Wade Mauger <wadeanthony0100@gmail.com>
2018-07-20 00:17:04 -0700

    Dont run test, only deploy

--- script/postdeploy.sh
+++ script/postdeploy.sh
@@ -1,37 +1,37 @@
-#!/bin/bash +x
-
-chromeDriverVersion=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-cd webdriverio-test
-curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
-
-if [ "$(uname)" == "Darwin" ]; then
-  # download and unpack the OSX version
-  echo "OSX detected, downloading chromedriver for Mac OS..."
-  curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_mac64.zip | funzip  > chromedriver
-  chmod +x chromedriver
-  ./chromedriver &
-  echo "Starting selenium server..."
-  java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
-elif [[ $CI ]]; then
-  # CircleCI ships with chromedriver v27
-  echo "CI runtime detected, skipping chromedriver download..."
-  chromedriver &
-  echo "Starting selenium server..."
-  java -jar selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
-fi
-#uncomment to use on linux
-#elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-#  # download and unpack the Linux version
-#  echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
-#  curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_linux64.zip | gzip -dc > chromedriver
-#  chmod +x chromedriver
-#  ./chromedriver &
-#  echo "Starting selenium server..."
-#  java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
-#fi
-
-echo "installing node dependancies..."
-npm i
-
-echo "Running tests..."
-npm test
+###!/bin/bash +x
+##
+#  chromeDriverVersion=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+#  cd webdriverio-test
+#  curl -O http://selenium-release.storage.googleapis.com/3.0/selenium-server-standalone-3.0.1.jar
+#
+#  if [ "$(uname)" == "Darwin" ]; then
+#    # download and unpack the OSX version
+#    echo "OSX detected, downloading chromedriver for Mac OS..."
+#    curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_mac64.zip | funzip  > chromedriver
+#    chmod +x chromedriver
+#    ./chromedriver &
+#    echo "Starting selenium server..."
+#    java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
+#  elif [[ $CI ]]; then
+#    # CircleCI ships with chromedriver v27
+#    echo "CI runtime detected, skipping chromedriver download..."
+#    chromedriver &
+#    echo "Starting selenium server..."
+#    java -jar selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
+#  fi
+#  #uncomment to use on linux
+#  #elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+#  #  # download and unpack the Linux version
+#  #  echo "Linux detected, downloading chromedriver for 64 Bit Linux OS's..."
+#  #  curl -L https://chromedriver.storage.googleapis.com/$chromeDriverVersion/chromedriver_linux64.zip | gzip -dc > chromedriver
+#  #  chmod +x chromedriver
+#  #  ./chromedriver &
+#  #  echo "Starting selenium server..."
+#  #  java -jar -Dwebdriver.chrome.driver=./chromedriver selenium-server-standalone-3.0.1.jar > seleniumServer.log 2>&1 &
+#  #fi
+#
+#  echo "installing node dependancies..."
+#  npm i
+##
+ # echo "Running tests..."
+ # npm test
